eb49cb34e1c5403c387a4b928b3335982e7a345d
Tit Petric <black@titpetric.com>
2025-12-02 17:16:43 +0100

    Bump go mod, tidy

--- evaluator/tokenizer_test.go
+++ evaluator/tokenizer_test.go
@@ -3,23 +3,19 @@ package evaluator
 import (
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/require"
 )
 
 func TestTokenizer(t *testing.T) {
 	tok, err := Tokenize("1px 0 2px rgb(1, 2, 3)")
+	require.NotEmpty(t, tok)
 	require.NoError(t, err)
-
-	//	spew.Dump(tok)
 
 	tok, err = Tokenize("Arial, sans-serif")
+	require.NotEmpty(t, tok)
 	require.NoError(t, err)
-
-	//	spew.Dump(tok)
 
 	tok, err = Tokenize("1px solid rgb(0, 0, 0)")
+	require.NotEmpty(t, tok)
 	require.NoError(t, err)
-
-	spew.Dump(tok)
 }
